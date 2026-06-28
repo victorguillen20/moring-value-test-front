66696addde5e08ed7018d4f8e5443cbe1d48e624
Victor Guillen <vguillen@viamatica.com>
2026-06-28 00:56:37 -0500

    feat(front): agregar provideHttpClient con interceptors placeholder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { correlationIdInterceptor } from './core/interceptors/correlation-id.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
+    provideHttpClient(withInterceptors([
+      correlationIdInterceptor,
+      authInterceptor,
+      errorInterceptor
+    ])),
     provideToastr({
       timeOut: 4000,
       positionClass: 'toast-top-right',
